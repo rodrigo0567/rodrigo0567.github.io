ce887364ffa82bff38d76b85818af5db851c5683
rodrigo0567 <rodrigo.verissimo2@academico.ufpb.br>
2025-02-03 20:14:03 -0300

    finalizar

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from werkzeug.utils import secure_filename
 import os
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 from dotenv import load_dotenv
 import json
 load_dotenv()
@@ -20,18 +22,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
-SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
-SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
-SPOTIPY_REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
-
-SCOPE = "playlist-modify-public playlist-modify-private"
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
-                                                client_secret=SPOTIPY_CLIENT_SECRET,
-                                                redirect_uri=SPOTIPY_REDIRECT_URI,
-                                                scope=SCOPE))
-
-
 creds_json = json.loads(os.getenv("GOOGLE_CREDENTIALS_JSON"))
 creds = Credentials.from_service_account_info(creds_json, scopes=["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"])
 
@@ -39,6 +29,16 @@ creds = Credentials.from_service_account_info(creds_json, scopes=["https://www.g
 client = gspread.authorize(creds)
 spreadsheet = client.open('CONVIDADOS CONFIRMADOS')  
 worksheet = spreadsheet.get_worksheet(0) 
+
+creds1_json = json.loads(os.getenv("GOOGLE_MUSIC_CREDENTIALS_JSON"))
+creds1 = Credentials.from_service_account_info(creds1_json, scopes=["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"])
+
+
+client1 = gspread.authorize(creds1)
+spreadsheet1 = client1.open('MÚSICA')  
+worksheet1 = spreadsheet1.get_worksheet(0) 
+
+
 questions = [
     {
         "question": "Quantos anos eu faço esse ano?",
@@ -101,91 +101,25 @@ def save_confirmation():
         worksheet.append_row([name])  # Adiciona o nome na próxima linha da planilha
     return redirect(url_for('main'))
 
-@app.route('/login')
-def login():
-    # Se o token já existe e não está expirado, redireciona para /add_music
-    if 'token_info' in session:
-        if not sp.auth_manager.is_token_expired(session['token_info']):
-            return redirect(url_for('add_music'))
-
-    # Gera a URL de autenticação do Spotify e redireciona
-    auth_url = sp.auth_manager.get_authorize_url()
-    return redirect(auth_url)
-
-
-@app.route('/callback')
-def callback():
-    code = request.args.get('code')
-
-    if not code:
-        return "Erro: Código de autenticação não recebido.", 400
-
-    print("Código recebido:", code)  # Adiciona um log
-
-    token_info = sp.auth_manager.get_access_token(code)
-
-    if not token_info:
-        return "Erro ao obter o token de acesso.", 400
-
-    session['token_info'] = token_info
-    return redirect(url_for('add_music'))
 
 
 @app.route('/add_music', methods=['GET', 'POST'])
 def add_music():
-    if 'token_info' not in session:
-        return redirect(url_for('login'))
-
-    # Garante que o token esteja atualizado antes de fazer qualquer requisição
-    token_info = session['token_info']
-    if sp.auth_manager.is_token_expired(token_info):
-        session['token_info'] = sp.auth_manager.refresh_access_token(token_info['refresh_token'])
-
-    sp_client = spotipy.Spotify(auth=session['token_info']['access_token'])
+    status = request.args.get('status')  # Captura o status da URL
 
     if request.method == 'POST':
-        music_name = request.form['music']
+        music_name = request.form.get('music')
+        artist_name = request.form.get('artist')  # Adiciona o campo do artista
 
-        try:
-            results = sp_client.search(q=music_name, type='track', limit=1)
-            if results['tracks']['items']:
-                track_uri = results['tracks']['items'][0]['uri']
-                playlist_id = os.getenv('PLAYLIST_ID')
-                sp_client.playlist_add_items(playlist_id, [track_uri])
-                return redirect(url_for('add_music', status='success'))
-            else:
-                return redirect(url_for('add_music', status='error'))
-        except Exception as e:
-            print(f"Erro: {e}")
-            return f"Ocorreu um erro: {e}"
+        if music_name and artist_name:
+            try:
+                worksheet1.append_row([music_name, artist_name])
+                return redirect(url_for('add_music', status='success'))  # Redireciona com status de sucesso
+            except Exception as e:
+                print(f"Erro ao salvar na planilha: {e}")
+                return redirect(url_for('add_music', status='error'))  # Redireciona com status de erro
     
-    return render_template('add_music.html', status=request.args.get('status'))
-
-
-@app.route('/add_photos', methods=['GET', 'POST'])
-def add_photos():
-    # Define a pasta de uploads dentro de static
-    app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploads')
-
-    # Cria a pasta se não existir
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-
-    if request.method == 'POST':
-        if 'photo' not in request.files:
-            return redirect(request.url)
-        photo = request.files['photo']
-        if photo.filename == '':
-            return redirect(request.url)
-        if photo and allowed_file(photo.filename):
-            filename = secure_filename(photo.filename)
-            photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('add_photos'))
-
-    # Lendo as fotos da pasta de uploads
-    photos = os.listdir(app.config['UPLOAD_FOLDER'])
-    photos = [f for f in photos if allowed_file(f)]  # Filtra apenas as imagens válidas
-    return render_template('add_photos.html', photos=photos)
+    return render_template('add_music.html', status=status)
 
 
 if __name__ == '__main__':
